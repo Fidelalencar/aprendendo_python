e09d390657fa3ab6f7df62c319dffd53ac3b2c91
Fidelalencar <44216783+Fidelalencar@users.noreply.github.com>
2019-06-16 15:42:23 -0300

    atualizando

--- aprendendo1/aprendendo5.py
+++ aprendendo1/aprendendo5.py
@@ -1,3 +1,28 @@
 #########################################################
-#### AULA 17 :  ##########
+#### AULA 17 :  Data Analysis ##########
+#########################################################
+
+## Data Science como a interseccao de 3 conjuntos:
+# Hacking skills
+# Math and Statistics knowledge
+# Substantive Expertise
+
+
+## Etapas de um data analysis project:
+# preparacao 
+# analise
+# reflexao
+# disseminacao
+
+
+## NumPy e' um pacote para estatistica computacional
+# numpy geralmente e' usado indiretamente atraves do pacote Pandas
+
+
+# Panda Series - sao como listas, mas seus dados so estao em um unico formato
+
+# min 20
+
+#########################################################
+#### AULA 18 :  Cleaning Data ##########
 #########################################################
